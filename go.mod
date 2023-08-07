61f2442d29da4548b9aee3160cbe9fde87383952
Josh Marchello <josh@jmarchello.com>
2023-08-07 09:51:54 -0500

    Read md files, extract front matter

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/jmarchello/golb
 
 go 1.20
 
-require github.com/gomarkdown/markdown v0.0.0-20230716120725-531d2d74bc12 // indirect
+require (
+	github.com/gomarkdown/markdown v0.0.0-20230716120725-531d2d74bc12 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- golb.go
+++ golb.go
@@ -1,60 +1,62 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "errors"
-  "time"
-  "io/ioutil"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
+	"regexp"
 
-  "github.com/gomarkdown/markdown"
-  "github.com/gomarkdown/markdown/html"
-  "github.com/gomarkdown/markdown/parser"
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/html"
+	"github.com/gomarkdown/markdown/parser"
+	"gopkg.in/yaml.v3"
 )
 
 type Page struct {
-  Path string
-  Title string
-  Date time.Time
-  Content []byte
-  IsHeaderLink bool
-  HeaderLinks []*Page
+	Path         string    `yaml:"path"`
+	Title        string    `yaml:"title"`
+	Date         time.Time `yaml:"date"`
+	IsHeaderLink bool      `yaml:"is_header_link"`
+	HtmlContent  []byte
+	MdContent    []byte
+	HeaderLinks  []*Page
 }
 
 func main() {
-  _, err := checkArgs()
-  if err != nil {
-    fmt.Println(err)
-    os.Exit(1)
-  }
+	_, err := checkArgs()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-  basePath := os.Args[1]
-  markdownData, err := readMdFiles(basePath)
-  if err != nil {
-    fmt.Println(err)
-    os.Exit(1)
-  }
+	basePath := os.Args[1]
+	pages, err := readMdFiles(basePath)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-
-  fmt.Println(len(markdownData))
-  fmt.Println(markdownData[0])
+	pages = extractFrontMatter(pages)
+	fmt.Printf("%+v\n", pages[0])
 }
 
 func checkArgs() (bool, error) {
-  if len(os.Args) < 2 {
-    return false, errors.New("No build directory provided")
-  }
+	if len(os.Args) < 2 {
+		return false, errors.New("No build directory provided")
+	}
 
-  fileInfo, err := os.Stat(os.Args[1])
-  if err != nil {
-    return false, err
-  }
+	fileInfo, err := os.Stat(os.Args[1])
+	if err != nil {
+		return false, err
+	}
 
-  if !fileInfo.IsDir() {
-    return false, errors.New("Provided path is not a directory")
-  }
+	if !fileInfo.IsDir() {
+		return false, errors.New("Provided path is not a directory")
+	}
 
-  return true, nil
+	return true, nil
 }
 
 func mdToHTML(md []byte) []byte {
@@ -71,32 +73,47 @@ func mdToHTML(md []byte) []byte {
 	return markdown.Render(doc, renderer)
 }
 
-func readMdFiles(basePath string) ([][]byte, error) {
-    var fileContents [][]byte
-    markdownPath := basePath + "/markdown/"
+func readMdFiles(basePath string) ([]Page, error) {
+	var pages []Page
+	markdownPath := basePath + "/markdown/"
 
-    files, err := ioutil.ReadDir(markdownPath)
-    if err != nil {
-      return nil, err
-    }
+	files, err := ioutil.ReadDir(markdownPath)
+	if err != nil {
+		return nil, err
+	}
 
-     // Loop through each file in the directory
-    for _, file := range files {
-      // Check if it's a regular file (not a directory)
-      if file.Mode().IsRegular() {
-        // Get the file path
-        filePath := markdownPath + file.Name()
+	// Loop through each file in the directory
+	for _, file := range files {
+		// Check if it's a regular file (not a directory)
+		if file.Mode().IsRegular() {
+			// Get the file path
+			filePath := markdownPath + file.Name()
 
-        // Read the file
-        data, err := ioutil.ReadFile(filePath)
-        if err != nil {
-            fmt.Println(err)
-            continue
-        }
+			// Read the file
+			data, err := ioutil.ReadFile(filePath)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
 
-        fileContents = append(fileContents, data)
-      }
-    }
+			pages = append(pages, Page{MdContent: data})
+		}
+	}
 
-    return fileContents, nil
+	return pages, nil
+}
+
+func extractFrontMatter(pages []Page) ([]Page) {
+	re := regexp.MustCompile(`(?s)^---\n(.+?)\n---\n`)
+	var newPages []Page
+
+	for _, page := range pages {
+		match := re.FindSubmatch(page.MdContent)
+		err := yaml.Unmarshal(match[1], &page)
+		if err != nil {
+			fmt.Println(err)
+		}
+		newPages = append(newPages, page)
+	}
+	return newPages
 }
