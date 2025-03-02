45d59cb622b4ddab317be8a07c5407cd738f634f
Josh Marchello <josh@jmarchello.com>
2025-03-01 22:29:15 -0600

    wip: markdown parser

--- golb.go
+++ golb.go
@@ -68,20 +68,6 @@ func checkArgs() (bool, error) {
 	return true, nil
 }
 
-func mdToHTML(md []byte) string {
-	// create markdown parser with extensions
-	extensions := parser.CommonExtensions | parser.AutoHeadingIDs | parser.NoEmptyLineBeforeBlock
-	p := parser.NewWithExtensions(extensions)
-	doc := p.Parse(md)
-
-	// create HTML renderer with extensions
-	htmlFlags := html.CommonFlags
-	opts := html.RendererOptions{Flags: htmlFlags}
-	renderer := html.NewRenderer(opts)
-
-	return string(markdown.Render(doc, renderer))
-}
-
 func readMdFiles(basePath string) ([]Page, error) {
 	var pages []Page
 	markdownPath := basePath + "/markdown/"
